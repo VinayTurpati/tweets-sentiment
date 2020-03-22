900831580bc0ec9e89e99db0cbfcba36fe076d5d
Vinay Turpati <42096696+VinayTurpati@users.noreply.github.com>
2020-03-22 13:41:04 +0530

    initial changes

---
+++ config.py
@@ -0,0 +1,9 @@
+API_key = "**********"
+API_secret_key = "**********"
+
+access_token = "**********"
+access_token_secret = "**********"
+
+num_tweets = 10
+word = "coronavirus"
+filter_retweets = True
